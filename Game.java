67b528376b474bf84815bc804e5957d58a8b85e4
Piyush <piyush0mishra@gmail.com>
2015-06-16 16:41:24 +0530

    used list

--- Game.java
+++ Game.java
@@ -1,57 +1,29 @@
 package in.piyush.wrestling;
 import java.util.*;
-import java.lang.Math;
 
 public class Game {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner userInput = new Scanner(System.in);
+		Scanner userInput = new Scanner(System.in);		
+		List<Wrestler> wrestlers = Wrestler.defaultDeck();
 		
-		System.out.println("Welcome to the Wrestling Virtual Cards Game \nThis is a two Players' game as of now!\nPlease Enter Players' names respectively");
+		Collections.shuffle(wrestlers);
 		
-		userInput.hasNext();
-		String player1Name = userInput.nextLine();
-		userInput.hasNext();
-		String player2Name = userInput.nextLine();
-		String playerAttacking;
-		String playerDefending;
+		List<Wrestler> playerOneCards = new ArrayList<Wrestler>();
+		List<Wrestler> playerTwoCards = new ArrayList<Wrestler>();
 		
-		System.out.println("*********************************************************\nDeciding who gets to bet first");
-		int chance = (int)(Math.random()*2);	
-		
-		if (chance == 1) {
-			playerAttacking = player1Name;
-			playerDefending = player2Name;			
+		int index=0;
+		for (Wrestler someWrestler : wrestlers) {
+			if (index%2 == 0){
+				playerOneCards.add(someWrestler);
+			} else {
+				playerTwoCards.add(someWrestler);
+			}
+			index++;
 		}
-		else {
-			playerAttacking = player2Name;
-			playerDefending = player2Name;			
-		}
-		System.out.println (playerAttacking+" gets to bet first\n*********************************************************");
 		
-		
-		int totalWrestlers =5;
-		Wrestler wrestler01 = new Wrestler("Undertaker", 3, 2.08, 136.0, 198, 45);
-		Wrestler wrestler02 = new Wrestler("Roman Reigns", 1, 1.93, 120.0, 87, 13);
-		Wrestler wrestler03 = new Wrestler("Daniel Bryan", 2, 1.78, 90.71, 149, 41);
-		Wrestler wrestler04 = new Wrestler("Dean Ambrose", 4, 1.92, 102.0, 51, 10);
-		Wrestler wrestler05 = new Wrestler("Brock Lesnar", 5, 1.91, 134.0, 71, 30);
-		Wrestler wrestler06 = new Wrestler("John Cena", 6, 1.85, 114.0, 1596, 359);
-		Wrestler wrestler07 = new Wrestler("Bray Wyatt", 7, 1.90, 136.0, 204, 288);
-		Wrestler wrestler08 = new Wrestler("Seth Rollins",8, 1.85, 93.0, 431, 537);
-		Wrestler wrestler09 = new Wrestler("Paige", 9, 1.73, 54.0, 315, 205);
-		Wrestler wrestler10 = new Wrestler("Nikki Bella", 10, 1.68, 57.0, 273, 193);
-		//TODO input more wrestlers... 
-		//Wrestler Wrestler6 = new Wrestler();
-		//Wrestler Wrestler7 = new Wrestler();
-		//Wrestler Wrestler8 = new Wrestler();
-		Wrestler wrestlers[] = {wrestler01, wrestler02, wrestler03, wrestler04, wrestler05, wrestler06, wrestler07, wrestler08, wrestler09, wrestler10};
-		
-		int attackingWrestler=(int)(Math.random()*totalWrestlers);
-		
-		System.out.println("Displaying "+playerAttacking+"'s top card");
-		wrestlers[attackingWrestler].display();
+		int lastIndex = playerOneCards.size()-1;
+		System.out.println(playerOneCards.get(lastIndex).toString());
 		
 		System.out.println("Choose an attributte to bet on...\n1.\tRank\n2.\tHeight\n3.\tWeight\n4.\tMatches Won\n5.\tMatches Lost\n6.\tTotal Matches Played");
 		int attributeChoice;
@@ -62,26 +34,20 @@ public class Game {
 			userInput.hasNextInt();
 			attributeChoice = userInput.nextInt();			
 		}
+
+		System.out.println(playerTwoCards.get(lastIndex).toString());
+		Wrestler defender=playerTwoCards.get(lastIndex);
 		
-		int defendingWrestler =(int)(Math.random()*totalWrestlers);
-		while (attackingWrestler == defendingWrestler) {
-			defendingWrestler =(int)(Math.random()*totalWrestlers);			
+		if(playerOneCards.get(lastIndex).wrestle(attributeChoice, defender)){
+			System.out.println("****Player 1 Wins****");
+			Wrestler removedCard = playerTwoCards.remove(lastIndex);
+			playerOneCards.add(removedCard);
 		}
-		
-		System.out.println("Displaying "+defendingWrestler+"'s top card");
-		wrestlers[defendingWrestler].display();
-		
-		Wrestler defender=wrestlers[defendingWrestler];
-		boolean result = wrestlers[attackingWrestler].wrestle(attributeChoice, defender);
-		
-		if (result) {
-			System.out.println("****"+playerAttacking+" Wins****");
+		else {
+			System.out.println("****Player 2 Wins****");
+			Wrestler removedCard = playerOneCards.remove(lastIndex);
+			playerTwoCards.add(removedCard);
 		}
-		else{
-			System.out.println("****"+playerDefending+" Wins****");
-		}
-		
 		userInput.close();
 	}
-
 }
--- Wrestler.java
+++ Wrestler.java
@@ -1,31 +1,62 @@
 package in.piyush.wrestling;
 
+import java.util.*;
+
 public class Wrestler {
 	public String name;
 	public int rank;
 	public double height;
 	public double weight;
-	public int matchesWon;
-	public int matchesLost;
-	public int totalMatches;
+	private int matchesWon;
+	private int matchesLost;
+	private int totalMatches;
 	
-	public Wrestler(String Name, int Rank, double Height, double Weight, int MatchesWon, int MatchesLost) {
-		this.name = Name;
-		this.rank = Rank;
-		this.height = Height;
-		this.weight = Weight;
-		this.matchesWon = MatchesWon;
-		this.matchesLost = MatchesLost;
-		this.totalMatches = MatchesWon+MatchesLost;
+	public Wrestler(String name, int rank, double height, double weight, int matchesWon, int matchesLost) {
+		
+		this.name = name;
+		this.rank = rank;
+		this.height = height;
+		this.weight = weight;
+		this.matchesWon = matchesWon;
+		this.matchesLost = matchesLost;
+		this.totalMatches = matchesWon + matchesLost;
+		
 	}
 	
-	public void display(){
-		System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
-		System.out.println ("Name = "+this.name+"\nRank = "+this.rank+"\nHeight = "+this.height+" m\nWeight = "+this.weight+" kgs\nMathces Won = "+this.matchesWon+"\nMathces Lost = "+this.matchesLost+"\nTotal Matches Played = "+this.totalMatches);
-		System.out.println("\n+++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+	public String toString() {
+		return "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"+"Name = "+this.name+"\nRank = "+this.rank+"\nHeight = "+this.height+" m\nWeight = "+this.weight+" kgs\nMathces Won = "+this.matchesWon+"\nMathces Lost = "+this.matchesLost+"\nTotal Matches Played = "+this.totalMatches+"\n+++++++++++++++++++++++++++++++++++++++++++++++++++++++++";
+	}
+	
+	public int setMatchesWon(int wins) throws Exception {
+		if(wins < this.matchesWon) {
+			throw new Exception("How come the matches won decrease!");
+		}
+		this.matchesWon = wins;
+		
+		this.totalMatches = this.matchesWon + this.matchesLost;
+		
+		return wins;
+	}
+	
+	public void incrementMatchesWon() {
+		this.matchesWon++;
+		
+		this.totalMatches++;
+	}
+	
+	public static List<Wrestler> defaultDeck() {
+		List<Wrestler> deck = new ArrayList<Wrestler>();
+		
+		deck.add(new Wrestler("Undertaker", 1, 10.5, 100.0, 40, 5));
+		deck.add(new Wrestler("Hulk Hogan", 2, 10.5, 100.0, 40, 5));
+		deck.add(new Wrestler("HHH", 5, 10.5, 100.0, 40, 5));
+		deck.add(new Wrestler("Kane", 17, 10.5, 100.0, 40, 5));
+		
+		return deck;
 	}
 	
 	public boolean wrestle (int choice, Wrestler obj) {
+
 		boolean result = false;
 		switch (choice) {
 		case 1 : if (this.rank < obj.rank)
@@ -74,5 +105,6 @@ public class Wrestler {
 		//Returns true if 'this' wrestler wins
 		//Returns false if 'obj' wrestler wins
 	}
-	
 }
+
+
